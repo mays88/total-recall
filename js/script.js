079975fe9e47cb7ec2fa8633edb5d8ee3849fe1f
Mays88 <antmays.ceo@gmail.com>
2024-04-09 11:22:37 -0400

    Section 2 Assignment complete

--- js/script.js
+++ js/script.js
@@ -170,29 +170,29 @@ if (num < 5) {
  *
  */
 
-// console.log(
-//     "----------Write a loop that will print out all the numbers from 0 to 10, inclusive----------"
-// );
+console.log(
+    "----------Write a loop that will print out all the numbers from 0 to 10, inclusive----------"
+);
 
-// for (let x = 0; x <= 10; x++) {
-//     console.log(x);
-// }
+for (let x = 0; x <= 10; x++) {
+    console.log(x);
+}
 
-// console.log(
-//     "----------Write a loop that will print out all the numbers from 10 up to and including 400----------"
-// );
+console.log(
+    "----------Write a loop that will print out all the numbers from 10 up to and including 400----------"
+);
 
-// for (let x = 10; x <= 400; x++) {
-//     console.log(x);
-// }
+for (let x = 10; x <= 400; x++) {
+    console.log(x);
+}
 
-// console.log(
-//     "----------Write a loop that will print out every third number starting with 12 and going no higher than 4000----------"
-// );
+console.log(
+    "----------Write a loop that will print out every third number starting with 12 and going no higher than 4000----------"
+);
 
-// for (let x = 12; x <= 4000; x += 3) {
-//     console.log(x);
-// }
+for (let x = 12; x <= 4000; x += 3) {
+    console.log(x);
+}
 
 /**
  *
@@ -200,23 +200,32 @@ if (num < 5) {
  *
  */
 
-// console.log(
-//     "----------Print out the numbers that are within the range of 1 - 100----------"
-// );
-// console.log(
-//     `----------Adjust your code to add a message next to even numbers only that says: "is an even number"----------`
-// );
-// for (let even = 1; even <= 100; even++) {
-//     if (even % 2 === 0) {
-//         console.log(`${even} is an even number.`);
-//     } else {
-//         console.log(even);
-//     }
-// }
+console.log(
+    "----------Print out the numbers that are within the range of 1 - 100----------"
+);
+console.log(
+    `----------Adjust your code to add a message next to even numbers only that says: "is an even number"----------`
+);
+for (let even = 1; even <= 100; even++) {
+    if (even % 2 === 0) {
+        console.log(`${even} is an even number.`);
+    } else {
+        console.log(even);
+    }
+}
 
-// console.log(`--For the numbers 0 - 100, print out "I found a <number>. High five!" if the number is a multiple of five Example Output:
-// I found a 5. High five!
-// I found a 10. High five!`);
+console.log(`--For the numbers 0 - 100, print out "I found a <number>. High five!" if the number is a multiple of five Example Output:
+I found a 5. High five!
+I found a 10. High five!
+
+--Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three Example Output:
+I found a 3. Three is a crowd
+I found a 5. High five!
+I found a 6. Three is a crowd
+I found a 9. Three is a crowd
+I found a 10. High five!
+
+-- For numbers divisible by both three and five, be sure your code prints both messages`);
 
 for (let num = 0; num <= 100; num++) {
     if (num % 5 === 0 && num % 3 === 0) {
@@ -232,11 +241,23 @@ for (let num = 0; num <= 100; num++) {
 
 console.log(`
 
---Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three Example Output:
-I found a 3. Three is a crowd
-I found a 5. High five!
-I found a 6. Three is a crowd
-I found a 9. Three is a crowd
-I found a 10. High five!
+Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
 
--- For numbers divisible by both three and five, be sure your code prints both messages`);
+Check your work! Your bank_account should have $55 in it.
+
+You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+
+Check your work! Your bank_account should have $10,100 in it.`);
+
+// Code for all above
+let toSumNum = 100;
+let bankAcct = 0;
+
+for (let x = 0; x <= toSumNum; x++) {
+    bankAcct += x;
+}
+console.log(`
+
+Your bank account currently has $${bankAcct * 2} in it.
+
+`);
