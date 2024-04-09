92eb969b629b7f679d87ce9f906bed1ba83d686d
Mays88 <antmays.ceo@gmail.com>
2024-04-09 10:05:39 -0400

    created loop that counts from 0 to 10

--- js/script.js
+++ js/script.js
@@ -163,3 +163,18 @@ if (num < 5) {
 } else {
     console.log("monkey");
 }
+
+/**
+ * SECTION 2:
+ * The Basics
+ *
+ */
+
+// Write a loop that will print out all the numbers from 0 to 10, inclusive
+
+for (let counter = 0; counter <= 10; counter++) {
+    console.log(counter);
+}
+
+// Write a loop that will print out all the numbers from 10 up to and including 400
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
